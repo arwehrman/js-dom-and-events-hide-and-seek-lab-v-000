a9277e16b036dad31d3c2dac37eb7af7bb4e6469
Amanda Wehrman <arwehrman@gmail.com>
2018-01-29 02:01:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+};
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+    for (let i = 0; i < rankedList.length; i++) {
+    let children = rankedList[i].children
+    for (let j =0; j < children.length; j++) {
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+};
+
+function deepestChild() {
+  const elements = document.getElementById('grand-node').querySelectorAll('div');
+  return elements[elements.length - 1];
+};
