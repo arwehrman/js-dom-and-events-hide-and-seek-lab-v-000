fd3628be8ebde28c100583851e1b15416bb40aaf
Amanda Wehrman <arwehrman@gmail.com>
2018-02-08 15:42:30 +0000

    Done.

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 };
 
-function increaseRankBy(n) {
+function increaseRankBy(num) {
+  //create a variable that pulls the class ranked list
   const rankedList = document.querySelectorAll('.ranked-list')
+  //for loop, iterate through children of ranked-list class
     for (let i = 0; i < rankedList.length; i++) {
-    let children = rankedList[i].children
-    for (let j =0; j < children.length; j++) {
-    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  let children = rankedList[i].children
+  //for loop, iterate through each child adding the num to exisiting number
+    for (let j = 0; j < children.length; j++) {
+      // for each child, using parseInt to change string to number before adding num
+      children[j].innerHTML = parseInt(children[j].innerHTML) + num
     }
   }
 };
