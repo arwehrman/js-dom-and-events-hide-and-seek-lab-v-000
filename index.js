7395c5ee88ab38865167268cf5d8c12ddf32c032
Amanda Wehrman <arwehrman@gmail.com>
2018-02-08 15:09:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('.ranked-list')
     for (let i = 0; i < rankedList.length; i++) {
     let children = rankedList[i].children
-    for (let j =0; j < children.length; j++) {
+    for (let j = 0; j < children.length; j++) {
     children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
