57d87bbb4928a18a73fed648bb0e72bdecc05141
arwehrman <arwehrman@gmail.com>
2018-03-06 22:16:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 };
 
-function nestedTarget() {
-  return document.querySelector('#nested .target')
-};
+function nestedTarget(){
+  return document.querySelector('#nested, .target')
 
-function increaseRankBy(num) {
-  //create a variable that pulls the class ranked list
+}
+
+function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('.ranked-list')
-  //for loop, iterate through children of ranked-list class
     for (let i = 0; i < rankedList.length; i++) {
   let children = rankedList[i].children
-  //for loop, iterate through each child adding the num to exisiting number
-    for (let j = 0; j < children.length; j++) {
-      // for each child, using parseInt to change string to number before adding num
-      children[j].innerHTML = parseInt(children[j].innerHTML) + num
+    for (let c = 0; c < children.length; c++) {
+      children[c].innerHTML = parseInt(children[c].innerHTML) + n
     }
   }
 };
 
-function deepestChild() {
+function deepestChild(){
   const elements = document.getElementById('grand-node').querySelectorAll('div');
-  return elements[elements.length - 1];
-};
+ return elements[elements.length - 1];
+}
